76063b02ead2e50235dd7e9d22c2ba7dfddbf1d0
tfwong36 <tfwong36@gmail.com>
2021-12-02 14:20:16 +0800

    [FizzBuzz][Jason] - Refactor: Making Output String static final String in Class

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,21 +1,33 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    private static final String Whizz = "Whizz";
+    private static final String Buzz = "Buzz";
+    private static final String Fizz = "Fizz";
+
     public String countOff(int orderNumber) {
         if (orderNumber % 3 == 0 && orderNumber % 5 == 0 && orderNumber % 7 == 0)
-            return "FizzBuzzWhizz";
+            return Fizz + Buzz + Whizz;
         if (orderNumber % 5 == 0 && orderNumber % 7 == 0)
-            return "BuzzWhizz";
+            return Buzz + Whizz;
         if (orderNumber % 3 == 0 && orderNumber % 7 == 0)
-            return "FizzWhizz";
+            return Fizz + Whizz;
         if (orderNumber % 3 == 0 && orderNumber % 5 == 0)
-            return "FizzBuzz";
+            return Fizz + Buzz;
         if (orderNumber % 7 == 0)
-            return "Whizz";
+            return Whizz;
         if (orderNumber % 5 == 0)
-            return "Buzz";
+            return Buzz;
         if (orderNumber % 3 == 0)
-            return "Fizz";
+            return Fizz;
         return String.valueOf(orderNumber);
+    }
+
+    public static void main(String args[]){
+        int range = 100;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int index = 0; index < range; index++){
+            System.out.println(fizzBuzz.countOff(index));
+        }
     }
 }

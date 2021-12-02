3eeacc96d49d5bd9c25abc95cd8553b77348af57
tfwong36 <tfwong36@gmail.com>
2021-12-02 13:58:11 +0800

    [FizzBuzz][Jason] - Feat: Test Case for normal order number

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,23 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case(){
+        //given
+        int orderNumber = 1;
+
+        //when
+        String result = fizzBuzz.countOff(orderNumber);
+
+        //then
+        assertEquals("1", result);
+
+    }
+
+
 }

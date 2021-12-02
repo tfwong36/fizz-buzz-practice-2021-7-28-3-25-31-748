b35cdf5cbc069a5e741f0f3452c0c56aef55bcab
tfwong36 <tfwong36@gmail.com>
2021-12-02 14:03:32 +0800

    [FizzBuzz][Jason] - Feat: Implement countOff function for multiple of 5 case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,8 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int orderNumber) {
+        if (orderNumber % 5 == 0)
+            return "Buzz";
         if (orderNumber % 3 == 0)
             return "Fizz";
         return String.valueOf(orderNumber);
